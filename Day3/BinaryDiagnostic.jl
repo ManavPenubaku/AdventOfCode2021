71eacd9cebabca0d8a890b47b1914374a7893f7e
Manav Penubaku <manav.prasad.27@gmail.com>
2021-12-03 21:00:19 +0100

    solved day 3 part 2

--- Day3/BinaryDiagnostic.jl
+++ Day3/BinaryDiagnostic.jl
@@ -12,14 +12,40 @@ function Part1(input)
             append!(epsilon,"0")
         else
             append!(gamma,"0")
-            append!(epsilon,"1")
+            append!(epsilon, "1");
         end
     end
     return parse(Int,join(gamma),base=2)*parse(Int,join(epsilon),base=2)
 end
 
 function Part2(input)
-
+    oxygen_generator_rating = deepcopy(input);
+    co2_scrubber_rate = deepcopy(input);
+    for n in 1:length(input[1])
+        if (size(oxygen_generator_rating,1) == 1)
+            break
+        end
+        oxygen_ones = findall(x->x.=="1",map(y->y[n][:],oxygen_generator_rating))
+        oxygen_zeros = findall(x->x.=="0",map(y->y[n][:],oxygen_generator_rating))
+        if (length(oxygen_ones) >= length(oxygen_zeros))
+            oxygen_generator_rating = oxygen_generator_rating[oxygen_ones]
+        else
+            oxygen_generator_rating = oxygen_generator_rating[oxygen_zeros]
+        end
+    end
+    for n in 1:length(input[1])
+        if (size(co2_scrubber_rate,1) == 1)
+            break
+        end
+        co2_ones = findall(x->x.=="1",map(y->y[n],co2_scrubber_rate))
+        co2_zeros = findall(x->x.=="0",map(y->y[n],co2_scrubber_rate))
+        if (length(co2_zeros) <= length(co2_ones))
+            co2_scrubber_rate = co2_scrubber_rate[co2_zeros]
+        else
+            co2_scrubber_rate = co2_scrubber_rate[co2_ones]
+        end
+    end
+    return parse(Int,join(oxygen_generator_rating[1]),base=2)*parse(Int,join(co2_scrubber_rate[1]),base=2)
 end
 
 sol1 = Part1(input);
