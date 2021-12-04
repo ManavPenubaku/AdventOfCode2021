3063f5889fe026f91f6b0202e9ea51cd942e8d31
Manav Penubaku <manav.prasad.27@gmail.com>
2021-12-04 13:04:32 +0100

    solved day 4 part 2

--- Day4/GiantSquid.jl
+++ Day4/GiantSquid.jl
@@ -20,28 +20,35 @@ function GetBingoBoards(lines)
     return bingo_boards
 end
 
-function Part1(bingo_boards,number_sequence)
+function FindBingoBoards(bingo_boards,number_sequence)
     matching_mask = BitArray(undef,size(bingo_boards))
     matching_mask .= 0;
     final_number = 0;
     sol1 = 0;
+    sol2 = 0;
     for n in 1:length(number_sequence)
         matching_mask = (matching_mask) .| (bingo_boards.== number_sequence[n])
         completed = findall(x->x.==5,sum(matching_mask,dims=2))
         append!(completed,findall(x->x.==5,sum(matching_mask,dims=1)))
-        println(completed)
-        println(n)
-        if (!isempty(completed))
+        if (length(completed) == 1)
             final_number = number_sequence[n]
             winning_board = bingo_boards[:,:,completed[1][3]]
-            println(winning_board)
             winning_mask = matching_mask[:,:,completed[1][3]]
             sol1 = final_number * sum(winning_board[findall(winning_mask.!=1)])
+        end
+        bingo_boards_filled = unique(map(x->x[3],completed));
+        if (length(bingo_boards_filled) == size(bingo_boards,3))
+            final_number = number_sequence[n]
+            losing_board = bingo_boards[:,:,bingo_boards_filled[end]]
+            losing_mask = matching_mask[:,:,bingo_boards_filled[end]]
+            sol2 = final_number * sum(losing_board[findall(losing_mask.!=1)])
             break
         end
     end
-    return sol1
+    return sol1,sol2
 end
 
 bingo_boards = GetBingoBoards(lines)
-sol1 =  Part1(bingo_boards,number_sequence)
+sol1,sol2 =  FindBingoBoards(bingo_boards,number_sequence)
+println("Solution to Part 1 is : ",sol1)
+println("Solution to Part 2 is : ",sol2)
