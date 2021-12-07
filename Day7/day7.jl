abf325f4c2d46723075cc7ae86380f662688edd3
Manav Penubaku <mpenubaku@rbionics.com>
2021-12-07 13:38:23 +0100

    solved day 7 puzzle

---
+++ Day7/day7.jl
@@ -0,0 +1,22 @@
+file = open("input.txt")
+lines = readlines(file)
+
+input = parse.(Int,String.(reduce(hcat,split.(lines,","))))
+
+function FindCheapestOutcome(input)
+    possible_endpoints = collect(minimum(input):1:maximum(input))
+    fuel_cost = map(x->sum(abs.(input.-x)),possible_endpoints);
+    return fuel_cost[findall(x->x.==minimum(fuel_cost),fuel_cost)]
+end
+
+function FindCheapestOutcome2(input)
+    possible_endpoints = collect(minimum(input):1:maximum(input))
+    fuel_cost = map(x->abs.(input.-x),possible_endpoints);
+    fuel_cost_progression = map(x->sum((x.*(2 .+ (x.-1)))./2),fuel_cost)
+    return fuel_cost_progression[findall(x->x.==minimum(fuel_cost_progression),fuel_cost_progression)]
+end
+
+sol1 = FindCheapestOutcome(input)
+println("Solution for Part 1 is : ",sol1[1])
+sol2 = FindCheapestOutcome2(input)
+println("Solution for Part 2 is : ",convert(Int,sol2[1]))
