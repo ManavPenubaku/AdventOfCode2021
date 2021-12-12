431c4763111903f632d304e596cb49f2b27b6dca
ManavPenubaku <manav.prasad.27@gmail.com>
2021-12-12 10:33:34 +0100

    obtained solution for day 10 part 2

--- Day10/SyntaxScoring.py
+++ Day10/SyntaxScoring.py
@@ -11,10 +11,19 @@ cost_dict = {
     '}': 1197,
     '>': 25137
 }
+correct_dict = {
+    '(': 1,
+    '[': 2,
+    '{': 3,
+    '<': 4
+}
 
 sol1 = 0
+sol2 = []
+incomplete_lines = []
 
-for line in lines: 
+for line in lines:
+    score = 0
     lines_without_chunks = line
     chunks_present_flag = True
     while chunks_present_flag:
@@ -26,6 +35,16 @@ for line in lines:
     result = map(lambda x: lines_without_chunks.find(x), illegal_characters)
     illegal_index = min([100 if x==-1 else x for x in result])
     if illegal_index != 100:
-       sol1 += cost_dict[lines_without_chunks[illegal_index]] 
+       sol1 += cost_dict[lines_without_chunks[illegal_index]]
+    else:
+        new_str = lines_without_chunks.strip()
+        reversed_line = new_str[::-1]
+        for character in range(len(reversed_line)):
+            score = score*5 + correct_dict[reversed_line[character]]
+        sol2.append(score)
 
 print("Solution to Part 1 is : ",sol1)
+middleIndex = int((len(sol2) - 1)/2)
+sol2.sort()
+print("Solution to Part 2 is : ",sol2[middleIndex])
+
