7b1890f5b244beebae75bf9ca4a1546a1d0ddea6
ManavPenubaku <manav.prasad.27@gmail.com>
2021-12-19 20:15:48 +0100

    solved day 15 puzzle

--- Day14/ExtendedPolymerization.jl
+++ Day14/ExtendedPolymerization.jl
@@ -1,30 +1,64 @@
-using StatsBase
-
 file = open("input.txt")
 lines = readlines(file)
 
 polymer_template = lines[1]
 
+polymer_template_dict = Dict()
+for n in 1:length(polymer_template)-1
+    if (haskey(polymer_template_dict,polymer_template[n:n+1]))
+        polymer_template_dict[polymer_template[n:n+1]] += 1
+    else
+        polymer_template_dict[polymer_template[n:n+1]] = 1
+    end
+end
+
 pair_insertions = reduce(hcat,split.(lines[3:end]," -> "))
 pair_insertion_dict = Dict(String.(pair_insertions[1,:]) .=> String.(pair_insertions[2,:]))
 
-function UpdatePolymer(polymer,pairs_dict)
-    updated_polymer = deepcopy(polymer)
-    for n in 1:length(polymer)-1
-        updated_polymer = updated_polymer[1:2*n-1] * pairs_dict[polymer[n:n+1]] * updated_polymer[2*n:end]
+function UpdatePolymer(polymer_dict,insertion_dict)
+    updated_polymer_dict = Dict()
+    pairs = collect(keys(polymer_dict))
+    for n in 1:length(pairs)
+        insertion = insertion_dict[pairs[n]]
+        new_pair_1 = pairs[n][1] * insertion
+        new_pair_2 = insertion * pairs[n][2] 
+        if (haskey(updated_polymer_dict,new_pair_1))
+            updated_polymer_dict[new_pair_1] += polymer_dict[pairs[n]]
+        else
+            updated_polymer_dict[new_pair_1] = polymer_dict[pairs[n]]
+        end
+        if (haskey(updated_polymer_dict,new_pair_2))
+            updated_polymer_dict[new_pair_2] += polymer_dict[pairs[n]]
+        else
+            updated_polymer_dict[new_pair_2] = polymer_dict[pairs[n]]
+        end
     end
-    return updated_polymer
+    return updated_polymer_dict
 end
 
 function RepeatUpdates(polymer_template,pair_insertion_dict,step_count)
     for iter in 1:step_count
         polymer_template = UpdatePolymer(polymer_template,pair_insertion_dict)
-        println(iter)
     end
-    character_counts = countmap(polymer_template)
-    println("Solution to Part 1 is : ",maximum(values(character_counts))-minimum(values(character_counts)))
+    println(sum(collect(values(polymer_template))))
+    element_dict = Dict()
+    pairs_final = collect(keys(polymer_template))
+    for n in 1:length(polymer_template)
+        if (haskey(element_dict,pairs_final[n][1]))
+            element_dict[pairs_final[n][1]] += polymer_template[pairs_final[n]]
+        else
+            element_dict[pairs_final[n][1]] = polymer_template[pairs_final[n]]
+        end
+        if (haskey(element_dict,pairs_final[n][2]))
+            element_dict[pairs_final[n][2]] += polymer_template[pairs_final[n]]
+        else
+            element_dict[pairs_final[n][2]] = polymer_template[pairs_final[n]]
+        end
+    end
+    return element_dict
 end
 
-RepeatUpdates(polymer_template,pair_insertion_dict,10)
-println(polymer_template)
-RepeatUpdates(polymer_template,pair_insertion_dict,40)
+character_count_p1 = RepeatUpdates(polymer_template_dict,pair_insertion_dict,10)
+println("Solution to Part 1 is : ", ceil(Int,(maximum(values(character_count_p1))-minimum(values(character_count_p1)))/2))
+character_count_p2 = RepeatUpdates(polymer_template_dict,pair_insertion_dict,40)
+println("Solution to Part 2 is : ", ceil(Int,(maximum(values(character_count_p2))/2-minimum(values(character_count_p2))/2)))
--- Day15/Chiton.jl
+++ Day15/Chiton.jl
@@ -10,13 +10,27 @@ function FindPath(ChitonDensity)
     risk_level = 0
     while destination_flag == 0
         if y_pos == size(ChitonDensity,1)
-
+            x_pos += 1
+            risk_level += ChitonDensity[y_pos][x_pos]
         elseif x_pos == size(ChitonDensity,2)
-
+            y_pos +=1 
+            risk_level += ChitonDensity[y_pos][x_pos]
         else
-            if (ChitonDensity[y_pos+1][x_pos] == ChitonDensity[y_pos][x_pos+1])
+            if (ChitonDensity[y_pos+1][x_pos] < ChitonDensity[y_pos][x_pos+1])
+                y_pos += 1
+                risk_level += ChitonDensity[y_pos][x_pos]
+            elseif (ChitonDensity[y_pos+1][x_pos] > ChitonDensity[y_pos][x_pos+1])
+                x_pos += 1
+                risk_level += ChitonDensity[y_pos][x_pos]
+            else
                 
             end
         end
+        if x_pos == size(ChitonDensity,1) && y_pos == size(ChitonDensity,2)
+            destination_flag = 1
+        end
     end
+    return risk_level
 end
+
+sol1 = FindPath(ChitonDensity)
