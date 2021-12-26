ef804adc002fd4674d8f8263cf2fabe2e2411970
ManavPenubaku <manav.prasad.27@gmail.com>
2021-12-26 16:08:08 +0100

    solved day 16 part 1

--- Day16/PacketDecoder.py
+++ Day16/PacketDecoder.py
@@ -1,5 +1,3 @@
-import copy
-
 lines = []
 with open('input.txt') as f:
     lines = f.readlines()
@@ -8,16 +6,53 @@ hexadecimal_input = lines[0].strip()
 bin_input = bin(int(hexadecimal_input,16))
 print(bin_input)
 
-def parse_packet(input_string,id,check_finish):
-    binary_input = copy.deepcopy(input_string)
+def evaluate_packet_1(type_id,prev,current):
+    print(type_id)
+    print(current)
+    eval = 0
+    if(type_id == 0):eval = prev+current
+    elif(type_id == 1):eval = prev*current
+    elif(type_id == 2):eval = min(prev,current)
+    elif(type_id == 3):eval = max(prev,current)
+    return eval 
+
+def evaluate_packet_2(type_id,prev,current):
+    print(type_id)
+    if(type_id == 5):eval = 1 if prev > current else 0
+    elif(type_id == 6):eval = 1 if prev < current else 0
+    elif(type_id == 7):eval = 1 if prev == current else 0
+    return eval
+
+def parse_packet(binary_input,id,check_finish,operator_id):
     packet_ver_start = 1
     cur_pos = 0
     packet_version_sum = 0
-    literal_value = []
-    packet_count = 0;
+    literal_value_list = []
+    literal_value = 0
+    packet_count = 0
+    packet_value = 0
+    prev_value = 0
+    compare_flag = 0
+    packet_id = 0
     while True:
-        print(binary_input)
-        print(packet_version_sum)
+        print(binary_input)  
+        print(packet_version_sum)                 
+        if(id == 2):
+            if(packet_count == check_finish):
+                break
+            elif(cur_pos+3 > len(binary_input) and check_finish < len(binary_input)):
+                cur_pos = min(cur_pos,len(binary_input)-1)
+                break
+            elif(cur_pos+3 > len(binary_input) and check_finish > len(binary_input)):
+                cur_pos = len(binary_input)-1   
+                break   
+        elif(id == 1 and cur_pos+3 > min(check_finish,len(binary_input))): 
+            if (check_finish > len(binary_input)):
+                cur_pos = len(binary_input)-1
+            else:
+                cur_pos = min(cur_pos,len(binary_input))
+            break
+        
         if(packet_ver_start == 1):
             packet_version_sum += int(binary_input[cur_pos:cur_pos+3],2)
             packet_ver_start = 0
@@ -30,24 +65,28 @@ def parse_packet(input_string,id,check_finish):
         elif(packet_id == 4):
             while True:
                 if len(binary_input[cur_pos:cur_pos+5]) == 5:
-                    literal_value.append(binary_input[cur_pos+1:cur_pos+5])
+                    literal_value_list.append(binary_input[cur_pos+1:cur_pos+5])
                 else:
+                    literal_value = int(''.join(literal_value_list),2)
+                    packet_count+=1
                     break
                 
                 if(int(binary_input[cur_pos],2) == 0):
+                    literal_value = int(''.join(literal_value_list),2)
                     cur_pos+=5
                     packet_ver_start = 1
                     packet_count+=1
+                    literal_value_list = []
                     break
                 else:
-                    cur_pos+=5
-        elif(packet_id != 4):
+                    cur_pos+=5    
+        else:
             length_type_id = int(binary_input[cur_pos],2)
             cur_pos+=1
             if(length_type_id == 0):
-                sub_packet_length = int(binary_input[cur_pos:cur_pos+15],2)
+                sub_packet_length = min(len(binary_input),int(binary_input[cur_pos:cur_pos+15],2))
                 cur_pos+=15
-                temp_sum,temp_pos = parse_packet(binary_input[cur_pos:cur_pos+sub_packet_length],1,sub_packet_length)
+                temp_sum,temp_pos,packet_value = parse_packet(binary_input[cur_pos:cur_pos+sub_packet_length],1,sub_packet_length,packet_id)
                 packet_count+=1
                 cur_pos += sub_packet_length
                 packet_version_sum += temp_sum
@@ -55,17 +94,23 @@ def parse_packet(input_string,id,check_finish):
             elif(length_type_id == 1):
                 sub_packet_count = int(binary_input[cur_pos:cur_pos+11],2)
                 cur_pos+=11
-                temp_sum,temp_pos = parse_packet(binary_input[cur_pos:],2,sub_packet_count)
+                temp_sum,temp_pos,packet_value = parse_packet(binary_input[cur_pos:],2,sub_packet_count,packet_id)
                 packet_count+=1
                 cur_pos += temp_pos+1
                 packet_version_sum += temp_sum
-                packet_ver_start = 1
+                packet_ver_start = 1        
                 
-        if(id==2 and (packet_count == check_finish)):
-            break
-        elif(id==1 and cur_pos+11 >= min(check_finish,len(binary_input))):
-            break
-    return packet_version_sum,cur_pos
+        if(operator_id < 4):
+            packet_value = evaluate_packet_1(operator_id,packet_value,literal_value)
+        elif(operator_id > 4):
+            if(compare_flag == 1):
+                packet_value = evaluate_packet_2(operator_id, prev_value, literal_value)
+            else:
+                prev_value = literal_value
+                compare_flag = 1  
+                
+    return packet_version_sum,cur_pos,packet_value
 
-sol1,sol2 = parse_packet(bin_input[2:], 1, len(bin_input[2:]))
+sol1,packet_pos,sol2 = parse_packet(bin_input[2:], 1, len(bin_input[2:]),4)
 print("Solution to Part 1 is : ",sol1)
+print("Solution to Part 2 is : ",sol2)
