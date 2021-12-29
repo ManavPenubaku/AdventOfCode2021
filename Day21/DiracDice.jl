f4c656b5addd6c162eeba9fc4d33196da62e22a8
ManavPenubaku <manav.prasad.27@gmail.com>
2021-12-29 12:55:03 +0100

    solved part 2 of day 21

--- Day21/DiracDice.jl
+++ Day21/DiracDice.jl
@@ -19,132 +19,61 @@ function PlayDiracDice1(position_p1,position_p2)
 end
 
 function PlayDiracDice2(position_p1,position_p2)
-    universe_positions = Dict(1 => [position_p1,position_p2])
-    universe_scores = Dict(1 => [0,0])
-    universe_dict_p1 = Dict([0,position_p1] => 1)
-    universe_dict_p2 = Dict([0,position_p2] => 1)
-    universe_outcomes = Dict(1 => 0,2 => 0)
+    universe_dict = Dict([0,position_p1,0,position_p2] => 1)
+    universe_outcomes = Dict(1 => 0, 2 => 0)
     roll_count = 1
-    temp_position = 0
     dice_values_dict = Dict(3 => 1,4 => 3,5 => 6,6 => 7,7 => 6,8 => 3,9 => 1)
-    while !isempty(universe_dict_p1)
-        temp_p1 = deepcopy(universe_dict_p1)
-        temp_p2 = deepcopy(universe_dict_p2)
-        temp_value = 0
+    while !isempty(universe_dict)
+        temp_dict = Dict()
+        temp_position = 0
+        temp_score = 0
         if (isodd(roll_count))
-            for (key,value) in temp_p1
-                if (key[1] < 21)
-                    for (dice_key,dice_value) in dice_values_dict
-                        temp_position = mod1(key[2] + dice_key,10)
-                        temp_score = key[1] + temp_position
-                        universe_dict_p1[[temp_score,temp_position]] = value*dice_value
-                        delete!(universe_dict_p1,key)
-                        # println(universe_dict_p1)
-                        if (temp_score >= 21)
-                            delete_key = collect(keys(filter(kv->kv.first[1] == key[1],universe_dict_p2)))
-                            if !isempty(delete_key)
-                                delete!(universe_dict_p2,delete_key[1])
-                            else
-                                println(value)
-                            end
-
-                        end
-                    end
-                else
-                    delete!(universe_dict_p1,key)
+            for (key,value) in universe_dict
+                for (dice_key,dice_value) in dice_values_dict
+                    temp_position = mod1(key[2] + dice_key,10)
+                    temp_score = key[1] + temp_position
+                    if (haskey(temp_dict,[temp_score,temp_position,key[3],key[4]]))
+                        temp_dict[[temp_score,temp_position,key[3],key[4]]] += value * dice_value
+                    else
+                        temp_dict[[temp_score,temp_position,key[3],key[4]]] = value * dice_value
+                    end           
                 end
             end
-            universe_outcomes[1] += temp_value
-            for (key,value) in universe_dict_p2
-                universe_dict_p2[key] = value * 27
-                # if (total_universe_count != 0) 
-                #     universe_dict_p2[key] = value * game_in_progress * 27 /total_universe_count
-                # else
-                #     delete!(universe_dict_p2,key)
-                # end
+            universe_dict = deepcopy(temp_dict)
+            games_won = filter(kv->kv.first[1] >= 21,universe_dict)
+            for (key,value) in games_won
+                delete!(universe_dict, key)
+                universe_outcomes[1] = universe_outcomes[1] + value
             end
         else
-            for (key,value) in temp_p2
-                if (key[1] < 21)
-                    for (dice_key,dice_value) in dice_values_dict
-                        temp_position = mod1(key[2] + dice_key,10)
-                        temp_score = key[1] + temp_position
-                        universe_dict_p2[[temp_score,temp_position]] = value*dice_value
-                        delete!(universe_dict_p2,key)
-                        if (temp_score >= 21)
-                            temp_value += value
-                            delete_key = collect(keys(filter(kv->kv.first[1] == key[1],universe_dict_p1)))
-                            if !isempty(delete_key)
-                                delete!(universe_dict_p1,delete_key[1])
-                            else
-                                println(value)
-                            end
-                        end
+            for (key,value) in universe_dict
+                for (dice_key,dice_value) in dice_values_dict
+                    temp_position = mod1(key[4] + dice_key,10)
+                    temp_score = key[3] + temp_position
+                    if (haskey(temp_dict,[key[1],key[2],temp_score,temp_position]))
+                        temp_dict[[key[1],key[2],temp_score,temp_position]] += value * dice_value
+                    else
+                        temp_dict[[key[1],key[2],temp_score,temp_position]] = value * dice_value
                     end
-                else
-                    delete!(universe_dict_p2,key)
                 end
             end
-            universe_outcomes[2] += temp_value
-            for (key,value) in universe_dict_p1
-                universe_dict_p1[key] = value * 27
-                # if (total_universe_count != 0)
-                #     universe_dict_p1[key] = value * 27 * game_in_progress /total_universe_count
-                # else
-                #     delete!(universe_dict_p1,key)
-                # end
+            universe_dict = deepcopy(temp_dict)
+            games_won = filter(kv->kv.first[3] >= 21,universe_dict)
+            for (key,value) in games_won
+                delete!(universe_dict, key)
+                universe_outcomes[2] = universe_outcomes[2] + value
             end
         end
         roll_count += 1
-        println(roll_count)
-        # universe_indices_1 = collect(1:1:length(universe_positions))
-        # universe_indices_2 = collect(length(universe_positions)+1:1:2*length(universe_positions))
-        # universe_indices_3 = collect(2*length(universe_positions)+1:1:3*length(universe_positions))
-        # temp_position_p1 = deepcopy(universe_positions)
-        # temp_scores_p1 = deepcopy(universe_scores)
-        # if isodd(ceil(roll_count/3))
-        #     setindex!.(Ref(universe_positions),map(x->[x[1].+1,x[2]],values(temp_position_p1)),universe_indices_1)
-        #     setindex!.(Ref(universe_positions),map(x->[x[1].+2,x[2]],values(temp_position_p1)),universe_indices_2)
-        #     setindex!.(Ref(universe_positions),map(x->[x[1].+3,x[2]],values(temp_position_p1)),universe_indices_3)
-        #     setindex!.(Ref(universe_scores),map(x->x,values(temp_scores_p1)),universe_indices_1)
-        #     setindex!.(Ref(universe_scores),map(x->x,values(temp_scores_p1)),universe_indices_2)
-        #     setindex!.(Ref(universe_scores),map(x->x,values(temp_scores_p1)),universe_indices_3)
-        #     if (mod(roll_count,3) == 0)
-        #         map!(x->[mod1(x[1],10),x[2]],values(universe_positions))
-        #         setindex!.(Ref(universe_scores),map((x,y)->[x[1]+y[1],x[2]],values(universe_scores),values(universe_positions)),[universe_indices_1;universe_indices_2;universe_indices_3])
-        #     end
-        # else
-        #     setindex!.(Ref(universe_positions),map(x->[x[1],x[2].+1],values(temp_position_p1)),universe_indices_1)
-        #     setindex!.(Ref(universe_positions),map(x->[x[1],x[2].+2],values(temp_position_p1)),universe_indices_2)
-        #     setindex!.(Ref(universe_positions),map(x->[x[1],x[2].+3],values(temp_position_p1)),universe_indices_3)
-        #     setindex!.(Ref(universe_scores),map(x->x,values(temp_scores_p1)),universe_indices_1)
-        #     setindex!.(Ref(universe_scores),map(x->x,values(temp_scores_p1)),universe_indices_2)
-        #     setindex!.(Ref(universe_scores),map(x->x,values(temp_scores_p1)),universe_indices_3)
-        #     if (mod(roll_count,3) == 0)
-        #         map!(x->[x[1],mod1(x[2],10)],values(universe_positions))
-        #         setindex!.(Ref(universe_scores),map((x,y)->[x[1],x[2]+y[2]],values(universe_scores),values(universe_positions)),[universe_indices_1;universe_indices_2;universe_indices_3])
-        #     end
-        # end
-        # result = filter(kv->(kv.second[1] >= 21 && !haskey(universe_outcomes,kv.first)),universe_scores)
-        # universe_keys_p1 = collect(keys(result))
-        # println(length(universe_positions))
-        # setindex!.(Ref(universe_outcomes),1,universe_keys_p1)
-        # map(x->delete!(universe_scores,x),universe_keys_p1)
-        # map(x->delete!(universe_positions,x),universe_keys_p1)
-        # result2 = filter(kv->(kv.second[2] >= 21 && !haskey(universe_outcomes,kv.first)),universe_scores)
-        # universe_keys_p2 = collect(keys(result2))
-        # setindex!.(Ref(universe_outcomes),2,universe_keys_p2)
-        # map(x->delete!(universe_scores,x),universe_keys_p2)
-        # map(x->delete!(universe_positions,x),universe_keys_p2)
-        # println(length(universe_positions))
     end
     return universe_outcomes
 end
 
 sol1 = PlayDiracDice1(4,7)
-println("Solution for Part 1 is : ",sol1)
+println("Solution to Part 1 is : ",sol1)
 
-sol2 = PlayDiracDice2(4,8)
+sol2 = PlayDiracDice2(4,7)
+println("Solution to Part 2 is : ", maximum(values(sol2)))
 
 
 
--- Day22/ReactorReboot.py
+++ Day22/ReactorReboot.py
@@ -12,7 +12,7 @@ state_list = []
 for line in lines:
     cuboid_list.append(p_coord.findall(line.strip()))
     state_list.append(p_state.findall(line.strip()))
-    
+
 region = [[[0 for k in xrange(101)]for j in xrange(101)]for i in xrange(101)]
 
 on_cubes = 0;
@@ -38,7 +38,49 @@ for n in range(len(state_list)):
 
 
 print(on_cubes)
-            
+
+def CheckCubeIntersection(cube1,cube2):
+    no_intersection = 0
+    xmin_int = cube1[0]
+    ymin_int  = cube1[2]
+    zmin_int = cube1[4]
+    if(cube1[0] > cube2[1]):
+        no_intersection+=1
+    elif(cube1[0] >= cube2[0]):
+        xmin_int = cube1[0]
+    elif(cube1[1] >= cube2[0]):
+        xmin_int = cube2[0]
+        
+    if(cube1[2] > cube2[3]):
+        no_intersection+=1
+    elif(cube1[2] >= cube2[2]):
+        ymin_int = cube1[2]
+    elif(cube2[2] <= cube1[3]):
+        ymin_int = cube2[2]
+    
+    if(cube1[4] > cube2[5]):
+        no_intersection+=1
+    elif(cube1[4] >= cube2[4]):
+        zmin_int = cube1[4]
+    elif(cube2[4] <= cube1[5]):
+        zmin_int = cube2[4]
+        
+    if (no_intersection == 3):
+        return 0,cube1
+    else:
+        return 1,[xmin_int,cube1[1],ymin_int,cube1[3],zmin_int,cube1[5]]
+
+non_intersecting_list = [list(map(lambda x: int(x),cuboid_list[0]))]
+for n in range(1,20,1):
+    cuboid_2 = list(map(lambda x: int(x),cuboid_list[n]))
+    for cuboid in non_intersecting_list:
+        int_flag,int_coord = CheckCubeIntersection(cuboid,cuboid_2)
+        if (int_flag == 0):
+            non_intersecting_list.append(cuboid_2)
+        else:
+            print(int_coord)
+        
+print(non_intersecting_list)
             
             
             
